6ba1be004257f4da8de7032d5a65dfd84d161e8a
jani8980 <jani.bavesh95@gmail.com>
2024-06-19 00:01:52 +0100

    done

---
+++ src/test/java/resources/testdata/TestData.java
@@ -0,0 +1,20 @@
+package resources.testdata;
+
+import org.testng.annotations.DataProvider;
+
+/**
+ * Created by Jay
+ */
+public class TestData {
+
+    @DataProvider(name = "DataSet")
+    public Object[][] getData(){
+        Object[][] data = new Object[][]{
+                {"Admin", "Admin", "Paul Collings", "Enable"},
+                {"Cassidy.Hope", "ESS", "Cassidy Hope", "Enable"},
+                {"Nina.Patel", "ESS", "Nina Patel", "Enable"},
+                {"Odis.Adalwin", "Admin", "Odis Adalwin", "Enable"}
+        };
+        return data;
+    }
+}
